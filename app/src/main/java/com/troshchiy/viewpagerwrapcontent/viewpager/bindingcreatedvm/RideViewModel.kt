16390f55c4989e5a2f5ba09f0860d7c8fe8e038d
Yvgent Troshchiy <yvgen.troshchiy@gmail.com>
2018-03-29 01:23:02 +0300

    Add textFun

--- app/src/main/java/com/troshchiy/viewpagerwrapcontent/viewpager/bindingcreatedvm/RideViewModel.kt
+++ app/src/main/java/com/troshchiy/viewpagerwrapcontent/viewpager/bindingcreatedvm/RideViewModel.kt
@@ -2,7 +2,10 @@ package com.troshchiy.viewpagerwrapcontent.viewpager.bindingcreatedvm
 
 import com.troshchiy.viewpagerwrapcontent.Ride
 
+
 class RideViewModel(val ride: Ride) {
+
+    fun textFun() = ride.text + ride.text
 
     val text = ride.text
 
